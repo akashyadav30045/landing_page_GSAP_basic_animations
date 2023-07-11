4ae667aa707868859f590c60d867601cb4c23118
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-07-11 22:20:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.to(".imagecontainer",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    stagger :2
+})
+
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:0
+})
+
+gsap.to(".text h1",{
+    delay:2,
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+})
